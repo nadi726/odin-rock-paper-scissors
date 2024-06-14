d2fac94240f8609b9ca09bbfd09fe897ff744c30
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-06-14 15:31:15 +0300

    Add computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello, world");
+function getComputerChoice() {
+    const choiceNum = Math.random() * 100 + 1
+
+    if (choiceNum <= 33) {
+        return "rock"
+    } else if (choiceNum <= 66) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
